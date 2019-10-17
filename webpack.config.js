5ac55e0d996600d8857a747803c8793e776b764f
Alex Lindeman <hello@ael.me>
2019-10-16 20:52:17 -0400

    configure webpack to use relative output publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: {
-    index: "./index.tsx",
-  },
+  entry: "./index.tsx",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
@@ -22,43 +19,13 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
-      baseUrl: "/",
-      title: "gitlab-ci-dashboard",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: [
-          {
-            loader: "ts-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(gif|jpg|png)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
-            },
-          },
-        ],
+        use: "ts-loader",
       },
     ],
   },
